43dd5db6ee9886e7f9379562c73ce5d58c130e67
Jose Barrios <jose@cognilab.com>
2017-02-16 22:11:58 -0800

    Renamed node to vertex, for clarity purposes. Added functionality, tests. Need to comment and add README. Pre-release around the corner

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-
 let _private = new WeakMap();
 
-class Node {
+
+class Vertex {
 
   static equal(n1, n2){
     n1 = JSON.stringify(n1);
@@ -11,39 +11,66 @@ class Node {
     return n1 === n2;
   }
 
-
   constructor(key, data){
     this.key = key || null;
     this.data = data || null;
+    this.degree = 0;
 
-    let privateProperties = {};
-    privateProperties.children = new Set();
-    _private.set(this, privateProperties);
+    let properties = {};
+    properties.neighbours = new Set();
+    _private.set(this, properties);
   }
 
-  addChild(node){
-    _private.get(this).children.add(node);
+  set degree(n){ }
+  get degree(){ return _private.get(this).neighbours.size; }
+
+  addNeighbour(neighbour){
+    _private.get(this).neighbours.add(neighbour);
   }
 
-  hasChild(node){
-    return _private.get(this).children.has(node);
+  hasNeighbour(neighbour){
+    return _private.get(this).neighbours.has(neighbour);
   }
 
-  getChild(target){
+  getNeighbour(target){
     let result = null;
-    for (let child of _private.get(this).children){
-      if(Node.equal(child, target)){
-        result = child;
+    for (let neighbour of _private.get(this).neighbours){
+      if(Vertex.equal(neighbour, target)){
+        result = neighbour;
         break;
       }
     }
     return result;
   }
 
-  removeChild(node){
-    _private.get(this).children.remove(node);
+  getNeighbourByKey(key){
+    let result = null;
+    for (let neighbour of _private.get(this).neighbours){
+      if(neighbour.key === key){
+        result = neighbour;
+        break;
+      }
+    }
+    return result;
+  }
+
+  removeNeighbour(neighbour){
+    _private.get(this).neighbours.remove(neighbour);
+  }
+
+  getNeighbours(){
+    return _private.get(this).neighbours;
+  }
+
+  delete(){
+    _private.get(this).neighbours.clear();
+    _private.get(this).degree = null;
+    _private = new WeakMap();
+    delete this.data;
+    delete this.key;
+    delete this;
   }
 
 }
 
-module.exports = Node;
+module.exports = Vertex;
--- tests/Node.tests.js
+++ tests/Node.tests.js
@@ -1,59 +1,149 @@
 'use strict'
 
-const Node = require('../index.js');
+const Vertex = require('../index.js');
 const assert = require('assert');
 
 const KEY = [1,2,3,4,5];
 const DATA = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE'];
 
-const EMPTY = new Node();
-describe('Node', function() {
+const EMPTY = new Vertex();
+describe('Vertex', function() {
 
-  describe('#constructor()', function() {
-    it('should create a new node', function() {
-      let node = new Node();
-      assert.deepEqual(node, {key:null, data:null} );
-      node = new Node(1);
-      assert.deepEqual(node, {key:KEY[0], data:null} );
-      node = new Node(1, 'ONE');
-      assert.deepEqual(node, {key:KEY[0], data:DATA[0]} );
-      node = new Node(KEY[1], DATA[1]);
-      assert.deepEqual(node, {key:KEY[1], data:DATA[1]} );
+  describe('Vertex.equal(n1, n2)', function() {
+    it('should return true of vertex are equal, false otherwise', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let target = new Vertex(KEY[0], DATA[0]);
+      assert.equal(Vertex.equal(vertex, target), true);
+      target = new Vertex(KEY[1], DATA[1]);
+      assert.equal(Vertex.equal(vertex, target), false);
     });
   });
 
-  describe('#data', function() {
+  describe('Vertex.constructor()', function() {
+    it('should create a new vertex', function() {
+      let vertex = new Vertex();
+      assert.deepEqual(vertex, {key:null, data:null} );
+      vertex = new Vertex(1);
+      assert.deepEqual(vertex, {key:KEY[0], data:null} );
+      vertex = new Vertex(1, 'ONE');
+      assert.deepEqual(vertex, {key:KEY[0], data:DATA[0]} );
+      vertex = new Vertex(KEY[1], DATA[1]);
+      assert.deepEqual(vertex, {key:KEY[1], data:DATA[1]} );
+    });
+  });
+
+});
+
+describe('let vertex = new Vertex()', function() {
+
+  describe('vertex.data', function() {
     it('should be able to get/set its key/data', function() {
-      let node = new Node();
-      node.key = KEY[1];
-      assert.deepEqual(node, {key:KEY[1], data:null} );
-      node.data = DATA[1];
-      assert.deepEqual(node, {key:KEY[1], data:DATA[1]} );
-      node.key = null;
-      node.data = null;
-      assert.deepEqual(node, {key:null, data:null} );
+      let vertex = new Vertex();
+      vertex.key = KEY[1];
+      assert.deepEqual(vertex, {key:KEY[1], data:null} );
+      vertex.data = DATA[1];
+      assert.deepEqual(vertex, {key:KEY[1], data:DATA[1]} );
+      vertex.key = null;
+      vertex.data = null;
+      assert.deepEqual(vertex, {key:null, data:null} );
 
     });
   });
 
-  describe('Node.equal', function() {
-    it('should return true of nodes are equal, false otherwise', function() {
-      let node = new Node(KEY[0], DATA[0]);
-      let target = new Node(KEY[0], DATA[0]);
-      assert.equal(Node.equal(node, target), true);
-      target = new Node(KEY[1], DATA[1]);
-      assert.equal(Node.equal(node, target), false);
+  describe('vertex.key', function() {
+    it('should be able to get/set its key/data', function() {
+      let vertex = new Vertex();
+      vertex.key = KEY[1];
+      assert.deepEqual(vertex, {key:KEY[1], data:null} );
+      vertex.data = DATA[1];
+      assert.deepEqual(vertex, {key:KEY[1], data:DATA[1]} );
+      vertex.key = null;
+      vertex.data = null;
+      assert.deepEqual(vertex, {key:null, data:null} );
+
     });
   });
 
-  describe('#addChild', function() {
-    it('should be able to add children to itself', function() {
-      let node = new Node(KEY[0], DATA[0]);
-      let child = new Node(KEY[1], DATA[1]);
-      node.addChild(child);
-      assert.equal(node.getChild(child), child);
+  describe('vertex.degree', function() {
+    it('should return the number of neighbours of the vertex', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let n1 = new Vertex(KEY[1], DATA[1]);
+      let n2 = new Vertex(KEY[2], DATA[2]);
+      let n3 = new Vertex(KEY[3], DATA[3]);
+      assert.deepEqual(vertex.degree, 0);
+      vertex.addNeighbour(n1);
+      assert.deepEqual(vertex.degree, 1);
+      vertex.addNeighbour(n2);
+      assert.deepEqual(vertex.degree, 2);
+      vertex.addNeighbour(n3);
+      assert.deepEqual(vertex.degree, 3);
     });
   });
 
+  describe('vertex.addNeighbour(node)', function() {
+    it('should add a neighbour to itself', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let child = new Vertex(KEY[1], DATA[1]);
+      vertex.addNeighbour(child);
+      assert.equal(vertex.getNeighbour(child), child);
+    });
+  });
 
+  describe('vertex.hasNeighbour(node)', function() {
+    it('should return if vertex has matching neighbour', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let child = new Vertex(KEY[1], DATA[1]);
+      vertex.addNeighbour(child);
+      assert.equal(vertex.hasNeighbour(child), true);
+    });
+  });
+
+  describe('vertex.getNeighbourByKey(key)', function() {
+    it('should return vertex matching key', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let child = new Vertex(KEY[1], DATA[1]);
+      vertex.addNeighbour(child);
+      assert.deepEqual(vertex.getNeighbourByKey(KEY[1]), child);
+    });
+  });
+
+  describe('vertex.getNeighbour(node)', function() {
+    it('should return vertex matching neighbour', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let child = new Vertex(KEY[1], DATA[1]);
+      vertex.addNeighbour(child);
+      assert.deepEqual(vertex.getNeighbour(child), child);
+    });
+  });
+
+  describe('vertex.getNeighbours()', function() {
+    it('should return a Set with all the vertex neighbours', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let n1 = new Vertex(KEY[1], DATA[1]);
+      let n2 = new Vertex(KEY[2], DATA[2]);
+      let n3 = new Vertex(KEY[3], DATA[3]);
+      vertex.addNeighbour(n1);
+      vertex.addNeighbour(n2);
+      vertex.addNeighbour(n3);
+      let s = new Set();
+      s.add(n1);
+      s.add(n2);
+      s.add(n3);
+      assert.deepEqual(vertex.getNeighbours(), s);
+    });
+  });
+
+  describe('vertex.delete()', function() {
+    it('should delete vertex and all properties', function() {
+      let vertex = new Vertex(KEY[0], DATA[0]);
+      let n1 = new Vertex(KEY[1], DATA[1]);
+      let n2 = new Vertex(KEY[2], DATA[2]);
+      let n3 = new Vertex(KEY[3], DATA[3]);
+      vertex.addNeighbour(n1);
+      vertex.addNeighbour(n2);
+      vertex.addNeighbour(n3);
+      vertex.delete();
+      assert.deepEqual(vertex, {});
+    });
+  });
 });
